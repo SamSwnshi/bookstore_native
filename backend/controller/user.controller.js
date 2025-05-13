babbad032a68eda55b8b77d4eeb7d80494f30017
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-05-14 00:29:08 +0530

    signup completed with jwt

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,6 +1,60 @@
-export const login = async(req,res) => {
-    
-}
-export const signup = async(req,res) => {
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-}
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
+export const login = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const signup = async (req, res) => {
+  try {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      return res.status(400).json({ message: "All Fields Required!" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ messsage: "Password should be at least  charcters long" });
+    }
+
+    const emailExisted = await User.findOne({ $or: [{ email }] });
+    if (emailExisted) {
+      return res.status(400).json({ message: "Email already Exists!" });
+    }
+    const usernameExisted = await User.findOne({ $or: [{ username }] });
+    if (usernameExisted) {
+      return res.status(400).json({ message: "Username already Exists!" });
+    }
+
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      username,
+      password: hashedPassword,
+      profileImage,
+    });
+
+    await user.save();
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Error in register routes", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,27 +1,33 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     username: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        minLength: 6,
+      type: String,
+      required: true,
+      minLength: 6,
     },
     profileImage: {
-        type: String,
-        default: ""
-    }
-},{timestamps: true})
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User",userSchema);
+
+
+const User = mongoose.model("User", userSchema);
 
 export default User;
