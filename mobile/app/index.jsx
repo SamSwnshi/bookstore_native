77e3c5ae9a040b3a2f7286770e68925e34947d0c
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-05-16 15:33:20 +0530

    Status bar

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router';
 
 const index = () => {
   return (
     <View style={styles.container}>
       <Text>index yo</Text>
+      <Link href="/(auth)">Login</Link>
+      <Link href="/(auth)/signup">Singup</Link>
+      
     </View>
   )
 }
